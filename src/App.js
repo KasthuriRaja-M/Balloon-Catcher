63437047abd27bd5eddd6475c2c575e63c5901c7
KasthuriRaja-M <pmraja003@gmail.com>
2025-08-22 20:34:34 +0530

    Refine project structure and enhance setup configuration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import BalloonCatcher from './components/BalloonCatcher';
+
+function App() {
+  return (
+    <div className="App">
+      <BalloonCatcher />
+    </div>
+  );
+}
+
+export default App;
